2703674fa32e32f057ceee4d4dc830099107191b
Duy Khanh <vuduykhanh3@gmail.com>
2021-06-23 23:07:20 +0700

    Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,22 +1,21 @@
 const Course = require('../models/Course');
-
+const { mutipleMongooseToObject } = require('../../util/mongoose')
 class SiteController {
-  // [GET] /news
-  index(req, res) {
+  // [GET] /
+  index(req, res, next) {
     // res.render('home');
-        Course.find({}, (err, course) =>{
-            if(!err) {
-                res.json(course)
-            }else {
-                
-            }
-        })
+        Course.find({})
+            .then(course => {
+                // Truyền dữ liệu vào handlebars
+                res.render('home', { course: mutipleMongooseToObject(course) })}
+            )
+            .catch(next)
   }
 
-  // [GET] /news/:slug
-  search(req, res) {
-    res.render('search');
-  }
-}
+    // [GET] /sreach
+    search(req, res) {
+            res.render('search');
+        }
+    }
 
 module.exports = new SiteController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 //
 const newsRouter = require('./news');
 const siteRouter = require('./site');
+const courseRouter = require('./course');
+
 
 function route(app) {
-  app.use('/news', newsRouter);
-  app.use('/', siteRouter);
+    app.use('/news', newsRouter);
+    app.use('/course', courseRouter);
+    app.use('/', siteRouter);
 }
 
 module.exports = route;
